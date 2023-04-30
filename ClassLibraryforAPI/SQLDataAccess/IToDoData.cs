08522ff32510756f32b3556d95dad624a00c017b
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-04-30 18:34:27 -0500

    created ToDo Data to hold CRUD methods forming an abstraction layer between the db and the controller.

---
+++ ClassLibraryforAPI/SQLDataAccess/IToDoData.cs
@@ -0,0 +1,14 @@
+﻿using DemoAPIClassLibrary.Models;
+
+namespace DemoAPIClassLibrary.SQLDataAccess
+{
+    public interface IToDoData
+    {
+        Task CompleteTodo(int assignedTo, int toDoId);
+        Task<ToDoModel?> Create(int assignedTo, string specificTask);
+        Task Delete(int assignedTo, int toDoId);
+        Task<List<ToDoModel>> GetAllAssigned(int assignedTo);
+        Task<ToDoModel?> GetOneAssigned(int assignedTo, int todoId);
+        Task UpdateTask(int assignedTo, int toDoId, string specificTask);
+    }
+}
---
+++ ClassLibraryforAPI/SQLDataAccess/ToDoData.cs
@@ -0,0 +1,79 @@
+﻿using DemoAPIClassLibrary.Models;
+
+namespace DemoAPIClassLibrary.SQLDataAccess
+{
+    //Abstraction Layer
+    public class ToDoData : IToDoData
+    {
+        private readonly ISqlDataAccess _sql;
+        public ToDoData(ISqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+        public Task<List<ToDoModel>> GetAllAssigned(int assignedTo)
+        {
+            return _sql.LoadData<ToDoModel,
+                                  dynamic>("dbo.spToDo_GetAllAssigned",
+                                new { AssignedTo = assignedTo },
+                                "DefaultConnection");
+
+        }
+        public async Task<ToDoModel?> GetOneAssigned(int assignedTo,
+                                                    int todoId)
+        {
+            var results = await _sql.LoadData<ToDoModel,
+                                  //dynamic needed to pass through an anynomous object to a parameter
+                                  dynamic>("dbo.spToDo_GetAllAssigned",
+                                //anynonomous object
+                                new { AssignedTo = assignedTo, ToDoId = todoId },
+                                "DefaultConnection");
+            return results.FirstOrDefault();
+        }
+        public async Task<ToDoModel?> Create(int assignedTo,
+                                                    string specificTask)
+        {
+            var results = await _sql.LoadData<ToDoModel,
+                                  //dynamic needed to pass through an anynomous object to a parameter
+                                  dynamic>("dbo.spToDo_Create",
+                                //anynonomous object
+                                new { AssignedTo = assignedTo, SpecificTask = specificTask },
+                                "DefaultConnection");
+            return results.FirstOrDefault();
+        }
+        public Task UpdateTask(int assignedTo, int toDoId, string specificTask)
+        {
+            return _sql.SaveData<dynamic>("dbo.spToDo_UpdateTask",
+                                new
+                                {
+                                    AssignedTo = assignedTo,
+                                    ToDoId = toDoId,
+                                    SpecificTask = specificTask
+                                },
+                                "DefaultConnection");
+
+        }
+        public Task CompleteTodo(int assignedTo, int toDoId)
+        {
+            return _sql.SaveData<dynamic>("dbo.spToDo_CompleteTodo",
+                                new
+                                {
+                                    AssignedTo = assignedTo,
+                                    ToDoId = toDoId
+                                },
+                                "DefaultConnection");
+
+        }
+        public Task Delete(int assignedTo, int toDoId)
+        {
+            return _sql.SaveData<dynamic>("dbo.spToDo_Delete",
+                                new
+                                {
+                                    AssignedTo = assignedTo,
+                                    ToDoId = toDoId
+                                },
+                                "DefaultConnection");
+
+        }
+
+    }
+}
